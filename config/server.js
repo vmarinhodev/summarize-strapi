9bd227ee6ab6f7cce12e232a84b51adcd1f77c54
Vitor Marinho <vitor@uw-s.nl>
2024-11-21 17:38:14 +0100

    add cloudinary envs

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env('HOST_URL'), // Sets the public URL of the application.
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS')
   },
