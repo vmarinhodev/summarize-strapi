39404a36c20bbe1706d378ba8ba9a92a0c62d355
Vitor Marinho <vitor@uw-s.nl>
2024-11-21 11:21:22 +0100

    ssl false

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     debug: false,
   },
