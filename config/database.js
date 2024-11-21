a7fadf605bf8dc0535803c47acf56509466c9cee
Vitor Marinho <vitor@uw-s.nl>
2024-11-21 11:18:11 +0100

    Add render.yaml

--- config/database.js
+++ config/database.js
@@ -8,9 +8,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED',false ),
-      },
+      ssl: env.bool('DATABASE_SSL', true),
     },
     debug: false,
   },
